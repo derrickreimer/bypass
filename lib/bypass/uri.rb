bd02d073dd74dc76d915436f4fde154e9096af4e
iantnance <iantnance@gmail.com>
2015-10-13 11:15:48 -0700

    refactor append_to_query_values to allow arrays

--- lib/bypass/uri.rb
+++ lib/bypass/uri.rb
@@ -3,7 +3,17 @@ require "addressable/uri"
 module Bypass
   class URI < Addressable::URI
     def append_to_query_values(params = {})
-      self.query_values = (query_values || {}).merge(params)
+      new_query_values = [].tap do |pairs|
+        params.each do |key, value|
+          pairs << "#{key}=#{value}"
+        end
+      end.join('&')
+
+      if self.query.nil?
+        self.query = "#{new_query_values}"
+      else
+        self.query += "&#{new_query_values}"
+      end
     end
   end
 end
--- test/bypass/uri_test.rb
+++ test/bypass/uri_test.rb
@@ -7,5 +7,23 @@ class Bypass::URITest < MiniTest::Test
       uri.append_to_query_values(:foo => "bar")
       assert_equal "http://www.getdrip.com?foo=bar", uri.to_s
     end
+
+    should "add mulitple values" do
+      uri = Bypass::URI.parse("http://www.getdrip.com")
+      uri.append_to_query_values(:foo => "bar", :name => "Ian")
+      assert_equal "http://www.getdrip.com?foo=bar&name=Ian", uri.to_s
+    end
+
+    should "add values to the query string if it starts with values" do
+      uri = Bypass::URI.parse("http://www.getdrip.com?foo=bar")
+      uri.append_to_query_values(:name => "Ian")
+      assert_equal "http://www.getdrip.com?foo=bar&name=Ian", uri.to_s
+    end
+
+    should "supoort arrays" do
+      uri = Bypass::URI.parse("http://www.getdrip.com?foo[]=bar1&foo[]=bar2")
+      uri.append_to_query_values(:name => "Ian")
+      assert_equal "http://www.getdrip.com?foo[]=bar1&foo[]=bar2&name=Ian", uri.to_s
+    end
   end
 end
