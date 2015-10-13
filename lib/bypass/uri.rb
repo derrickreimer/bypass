53e9926b642c564a2a96a12b93f5c08ac5eec7a0
iantnance <iantnance@gmail.com>
2015-10-13 12:57:43 -0700

    encode param keys

--- lib/bypass/uri.rb
+++ lib/bypass/uri.rb
@@ -3,7 +3,7 @@ require "addressable/uri"
 module Bypass
   class URI < Addressable::URI
     def append_to_query_values(params = {})
-      pairs = params.keys.map { |key| "#{key}=#{Addressable::URI.encode(params[key])}" }
+      pairs = params.keys.map { |key| "#{Addressable::URI.encode(key.to_s)}=#{Addressable::URI.encode(params[key])}" }
       pairs.unshift(query) if query
       self.query = pairs.join("&")
     end
