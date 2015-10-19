35bf88875ea66468e96f7e35bdab08b0eb9d70df
Derrick Reimer <derrickreimer@gmail.com>
2015-10-19 10:04:27 -0700

    A little refactoring and polish

--- lib/bypass/uri.rb
+++ lib/bypass/uri.rb
@@ -3,7 +3,12 @@ require "addressable/uri"
 module Bypass
   class URI < Addressable::URI
     def append_to_query_values(params = {})
-      pairs = params.keys.map { |key| "#{Addressable::URI.encode(key.to_s)}=#{Addressable::URI.encode(params[key])}" }
+      pairs = params.keys.map do |raw_key|
+        key = self.class.encode(raw_key.to_s)
+        val = self.class.encode(params[raw_key])
+        [key, val].join("=")
+      end
+
       pairs.unshift(query) if query
       self.query = pairs.join("&")
     end
--- test/bypass/uri_test.rb
+++ test/bypass/uri_test.rb
@@ -14,19 +14,31 @@ class Bypass::URITest < MiniTest::Test
       assert_equal "http://www.getdrip.com?foo=bar&name=Ian", uri.to_s
     end
 
-    should "add values to the query string if it starts with values" do
+    should "append to the existing query string" do
       uri = Bypass::URI.parse("http://www.getdrip.com?foo=bar")
       uri.append_to_query_values(:name => "Ian")
       assert_equal "http://www.getdrip.com?foo=bar&name=Ian", uri.to_s
     end
 
-    should "supoort arrays" do
+    should "support array-style keys" do
       uri = Bypass::URI.parse("http://www.getdrip.com?foo[]=bar1&foo[]=bar2")
       uri.append_to_query_values(:name => "Ian")
       assert_equal "http://www.getdrip.com?foo[]=bar1&foo[]=bar2&name=Ian", uri.to_s
     end
 
-    should "handle encoding" do
+    should "not attempt to merge duplicate keys" do
+      uri = Bypass::URI.parse("http://www.getdrip.com?name=Ian")
+      uri.append_to_query_values(:name => "Derrick")
+      assert_equal "http://www.getdrip.com?name=Ian&name=Derrick", uri.to_s
+    end
+
+    should "encode keys" do
+      uri = Bypass::URI.parse("http://www.getdrip.com")
+      uri.append_to_query_values("first name" => "Ian")
+      assert_equal "http://www.getdrip.com?first%20name=Ian", uri.to_s
+    end
+
+    should "encode values" do
       uri = Bypass::URI.parse("http://www.getdrip.com")
       uri.append_to_query_values(:name => "Ian Nance")
       assert_equal "http://www.getdrip.com?name=Ian%20Nance", uri.to_s
